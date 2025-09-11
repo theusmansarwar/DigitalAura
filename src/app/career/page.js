d13f9fb12869fc61876b5ef33a6b67c04fb28d5a
Muhammad Usman Sarwar <105904144+theusmansarwar@users.noreply.github.com>
2025-09-11 12:33:48 +0500

    contact and career page added

---
+++ src/app/career/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ContactInfo from "../components/Contact/ContactInfo";
+import ContactUs from "../components/Contact/ContactUs";
+import JoinUs from "../components/career/JoinUs";
+
+
+export const metadata = {
+  title: "Creative Services | Digital Aura",
+  description:
+    "Elevate your brand with Digital Aura’s creative expertise. Smart design in logos, ads, and UI/UX ensures trust, style, and powerful business growth.",
+};
+
+const page = () => {
+  return (
+    <div>
+   <JoinUs/>    </div>
+  );
+};
+
+export default page;
---
+++ src/app/contact-us/page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ContactInfo from "../components/Contact/ContactInfo";
+import ContactUs from "../components/Contact/ContactUs";
+
+
+export const metadata = {
+  title: "Creative Services | Digital Aura",
+  description:
+    "Elevate your brand with Digital Aura’s creative expertise. Smart design in logos, ads, and UI/UX ensures trust, style, and powerful business growth.",
+};
+
+const page = () => {
+  return (
+    <div>
+     <ContactInfo/>
+<ContactUs/>    </div>
+  );
+};
+
+export default page;
